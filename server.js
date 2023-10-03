2ac3e7b9500818648549c6c53952da844f235544
Johannes Hötter <johannes.hoetter@kern.ai>
2023-10-03 15:28:07 +0200

    change to port 3000

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.post('/api/convertPdfToMd', upload.single('file'), async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
